c7dea300f3e9811e5df1eaf5199e8ab048cd7350
Talha Saleem <talha.sameed@gmail.com>
2023-03-14 01:52:18 +0500

    Major design revamp

--- pages/index.js
+++ pages/index.js
@@ -53,35 +53,42 @@ const Home = () => {
   }
   
   return (
-    <div className='flex-col text-white-1 flex bg-gray-1 w-full h-full gap-8 md:gap-16 items-center'>
+    <div className='flex-col h-ful absolute text-white-1 px-8 md:px-16 flex bg-gray-1 w-full gap-8 md:gap-16 items-center'>
       
       {/* Header */}
-      <div className='text-title font-abuzar relative'>تخیل</div>
+      <div className='flex items-center flex-row-reverse justify-between w-full py-4'>
+        <div className={`text-lg font-['Divangiry'] relative`}>ت خ ی ل</div>
+        <div className='text-gray-1'>About</div>
+      </div>
+
+      {/* Menu */}
+      <div className='flex items-end md:border-b border-white-2 border-opacity-20 md:items-center flex-col md:flex-row-reverse md:justify-between w-full'>
+        
+        {/* Input */}
+        <input name='text' placeholder='یہاں ٹائپ کریں' className='py-1 text-right h-auto md:px-6 bg-gray-1 w-full placeholder:text-white-2 placeholder:text-opacity-20 outline-none box-border text-lg rounded-sm align-middle text-white-2 border-b border-white-2 border-opacity-30 md:border-opacity-0  focus:border-opacity-20 ' type="text" onChange={onChange} value={value}/> 
+        
+        {/* Color picker */}
+        <button className="w-fit inline-block md:w-auto text-md bg-gray-1 border border-gray-5 rounded-full px-8 py-1 mt-2" onClick={() => toggle(!isOpen)}>Color</button>
+      </div>
+        
+      {/* Selector */}
+      {isOpen ? <HexColorPicker color={color} onChange={setColor} /> : ""}
       
       {/* Inner */}
-      <div className='px-2 items-center flex justify-center w-full flex-col'>
-
-        {/* Input */}
-        <input name='text' className='py-1 text-right h-auto md:px-6 bg-gray-2 md:w-3/6 placeholder:text-white-2 outline-none box-border text-lg rounded-sm align-middle text-white-2 border border-white-2 border-opacity-10 focus:border-opacity-20 ' type="text" onChange={onChange} value={value}/>
-          
-        {/* Color picker */}
-        <button className="rounded-sm w-full md:w-auto text-md bg-gray-3 px-8 py-1 mt-2" onClick={() => toggle(!isOpen)}>Color</button>
-
-        {/* Selector */}
-        {isOpen ? <HexColorPicker color={color} onChange={setColor} /> : ""}
+      <div className='items-center flex w-full flex-col'>
         
         {/* Display fonts */}
-        <div className='mt-20 flex text-right flex-col items-center overflow-y-auto w-full h-full'>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Qamri']`} onClick={() => download("Qamri", value, color)}>{value}</div>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Divangiry']`} onClick={() => download("Divangiry", value, color)}>{value}</div>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Tasmeem']`} onClick={() => download("Tasmeem", value, color)}>{value}</div>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Kamran']`} onClick={() => download("Kamran", value, color)}>1{value}</div>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Abuzar']`} onClick={() => download("Abuzar", value, color)}>{value}</div>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Sadaf']`} onClick={() => download("Sadaf", value, color)}>{value}</div>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Ubaid']`} onClick={() => download("Ubaid", value, color)}>{value}</div>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Tehreeri']`} onClick={() => download("Tehreeri", value, color)}>2{value}</div>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Qalm']`} onClick={() => download("Qalm", value, color)}>3{value}</div>
-          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate w-3/4 font-['Shekastah']`} onClick={() => download("Shekastah", value, color)}>{value}</div>
+        <div className='mt-20 flex text-right flex-col overflow-y-auto w-full h-full'>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Qamri']`} onClick={() => download("Qamri", value, color)}>{value}</div>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Divangiry']`} onClick={() => download("Divangiry", value, color)}>{value}</div>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Tasmeem']`} onClick={() => download("Tasmeem", value, color)}>{value}</div>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Kamran']`} onClick={() => download("Kamran", value, color)}>{value}</div>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Abuzar']`} onClick={() => download("Abuzar", value, color)}>{value}</div>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Sadaf']`} onClick={() => download("Sadaf", value, color)}>{value}</div>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Ubaid']`} onClick={() => download("Ubaid", value, color)}>{value}</div>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Tehreeri']`} onClick={() => download("Tehreeri", value, color)}>{value}</div>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Qalm']`} onClick={() => download("Qalm", value, color)}>{value}</div>
+          <div style={{color: color}} className={`text-h1 cursor-pointer border-b-gray-5 py-4 border-b truncate font-['Shekastah']`} onClick={() => download("Shekastah", value, color)}>{value}</div>
         </div>
       </div>
     </div>
